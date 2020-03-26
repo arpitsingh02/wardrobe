ca7f89f6572755e824ffb7ae5d797a0cb0c909f4
daxy <arpit22201@gmail.com>
2020-03-26 21:19:45 +0530

    Additional info added

--- Wardrobe/core/models.py
+++ Wardrobe/core/models.py
@@ -43,6 +43,7 @@ class Item(models.Model):
     slug = models.SlugField()
     description = models.TextField()
     image = models.ImageField()
+    addinfo=models.CharField(max_length=1000,default="No additional info")
 
     def __str__(self):
         return self.title
